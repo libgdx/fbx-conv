4745ef4cb3ff01f655c13610fcc1bf10bb8a7f22
Will Davies <wdavies973@gmail.com>
2019-02-15 13:33:26 -0600

    Update FbxConvCommand.h

--- src/FbxConvCommand.h
+++ src/FbxConvCommand.h
@@ -52,7 +52,7 @@ struct FbxConvCommand {
 			const int len = (int)strlen(arg);
 			if(len > 1 && arg[0] == '-') {
 				if(arg[1] == '?')
-				    help = true;
+				    	help = true;
 				else if (arg[1] == 'f')
 					settings->flipV = true;
 				else if (arg[1] == 'v')
