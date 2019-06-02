5cf41819f5e49390f75964edf890a0d81c11dd62
Tom-Ski <tomaszwojciechowski@hotmail.co.uk>
2019-06-02 18:02:02 +0100

    Windows build, update comments

--- generate_vs2013.bat
+++ generate_vs2013.bat
@@ -2,7 +2,7 @@
 for %%X in (premake5.exe) do (set FOUND=%%~$PATH:X)
 if not defined FOUND (
 echo 'premake5' not found in your path.
-echo Premake can be downloaded from http://industriousone.com/premake/
+echo Premake can be downloaded from https://premake.github.io/download.html
 exit /b
 )
 
